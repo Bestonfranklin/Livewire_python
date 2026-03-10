383a3f8bdecc34a02ac6bcbdec7821d919d1ae00
Bestonfranklin <110010423+Bestonfranklin@users.noreply.github.com>
2026-03-10 13:26:37 +0530

    Create if_else program.py

---
+++ if_else program.py
@@ -0,0 +1,120 @@
+#1.Length and Breadth
+"""Length = int(input("Enter the number:"))
+Breadth = int(input("Enter the number:"))
+if Length==Breadth:
+    print("It is a square")
+else:
+    print("It is a rectangle")"""
+
+#2.Check if its Square
+"""a=int(input("Enter the number:"))
+a**=2
+if a<=50:
+    print("The square value is Below 50")
+else:
+    print("The square value is above 50")"""
+#2.output
+"""Enter the number:13
+The square value is above 50"""
+
+#3.subtraction
+"""a=int(input("Enter First number:"))
+b=int(input("Enter second number:"))
+sub=a-b
+if sub==0:
+    print("Answer is 0")
+else:
+    print("Answer is not 0")"""
+#3.Output
+"""Enter First number:12
+Enter second number:45
+Answer is not 0
+
+Enter First number:12
+Enter second number:12
+Answer is 0"""
+
+#4.pass or fail
+"""a=int(input("Enter the Mark:"))
+if a>=50:
+    print("pass")
+else:
+    print("fail")"""
+#4.output
+"""Enter a Mark:35
+fail
+Enter a Mark:52
+pass"""
+
+#5.divisible by 10
+"""a=int(input("Enter a number:"))
+if a%10==0:
+    print("divisible by 10")
+else:
+    print("not divisible by 10")"""
+#5.output
+"""Enter a number:30
+divisible by 10
+Enter a number:47
+not divisible by 10"""
+
+#6.print largest number
+"""a=int(input("Enter a two digit number"))
+b=a%10
+c=a//10
+if b==c:
+    print("Both are equal")
+elif b>c:
+    print("The biggest digit is: ",b)
+else:
+    print("The biggest digit is: ",c)"""
+#6.output
+"""Enter a two digit number: 12
+The biggest digit is  2
+Enter a two digit number: 67
+The biggest digit is  7"""
+
+#7.go out and play
+"""a = int(input("Enter the number:"))
+if a==1:
+    print("You can go out and play")
+else:
+    print("You cannot go out and play")"""
+#7.output
+"""Enter the number:12
+You cannot go out and play
+Enter the number:1
+You can go out and play"""
+
+#8.accept the choice exam
+"""a = int(input("Enter the number:"))
+if a==1:
+    print("The exam will be easy")
+else:
+    print("The exam will be difficult")"""
+#8.output
+"""Enter the number:3
+The exam will be difficult
+Enter the number:1
+The exam will be easy"""
+
+#9 Calculator
+"""a=int(input("Enter number 1:"))
+b=int(input("Enter number 1:"))
+c=str(input("Choose an operation +,-,*,/: "))
+if c=="+":
+    print("The addition is:",a+b)
+elif c=="-":
+    print("The subtraction is:",a-b)
+elif c=="*":
+    print("The multiplication is:",a*b)
+elif c=="/":
+    print("The division is:",a/b)
+else:
+    print("Wrong operation")"""
+#9.Output
+"""Enter number 1:12
+Enter number 1:23
+Choose an operation +,-,*,/: *
+The multiplication is: 276"""
+
